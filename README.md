8d2783e0f5e52ed72440574857701db684c499fe
Josevillarr <jsuperman5@hotmail.com>
2021-12-16 11:55:03 -0800

    added text to the ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Password-Generator-Challenge
+
+## Create a password generator that does the following:
+
+* Confirms with the user, the types of characters they want to include.
+
+* Asks and confirms the length of the password be between 8 and 128 characters, max.
+
+* Includes at least one of each character that gets confirmed by the user, within the password.
+
+* Generates a random password everytime the 'Generate Password' button is clicked.
--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 // Assignment code here
-
+var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharChar = ["!", "@", "#", "$", "%", "&", "*"];
+// might have to write a function to declare random numbers from 0 - 128
+var numeric = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// start defining function
+var getPreference = function() {
+    console.log("function working")
+
+    var length = parseInt(prompt("How many characters?"));
+    console.log(length);
+    var lowerCase = confirm("Do you want lower case");
+    var upperCase = confirm("Do you want upper case?");
+    var specialCharacters = confirm("Do you want to use special characters?");
+    var numeric = confirm("Do you want numbers?");
+    
+    var preferencesObj = {
+        length: length,
+        lowerCase: lowerCase,
+        upperCase: upperCase,
+        specialCharacters: specialCharacters,
+        numeric: numeric
+
+    }
+
+    return preferencesObj;
+}
+
+// define generate password
+var generatePassword = function() {
+    var userPreference = getPreference();
+    console.log(userPreference);
+    // create a varible for possible characters based on user preferences
+    var possibleCharacters = [];
+    var generatedPassword = [];
+    
+    return generatedPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
